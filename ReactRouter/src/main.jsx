523d752a00c1c09bbeabc3b8643ef4cdf21daac4
CBTCIP <sonisrushti12@gmail.com>
2025-11-16 01:05:25 +0530

    chore: created structure and implemented header, footer and routing concepts

--- ReactRouter/src/main.jsx
+++ ReactRouter/src/main.jsx
@@ -2,9 +2,30 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Layout from './Layout.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {
+        path: "",
+        element: <Home/>
+      } ,
+      {
+        path: "/about",
+        element: <About />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
